f0330a4e0715ac8c346f54c320fc031f34cd0b32
pillank1 <nikita37pobeditel@mail.ru>
2021-08-27 08:50:39 +0300

    test

--- bot_main.py
+++ bot_main.py
@@ -2,7 +2,7 @@ import vk_api.vk_api, vk
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
 from bot.config import main_token  # создать config.py спеременной main_token='написать токен здесь'
 from parser_url.parser import Parser
-from bot.bot_command import COMMAND_INPUT_URL
+from bot.bot_command import COMMAND_INPUT_URL, COMMAND_HELP
 
 vk_session = vk_api.VkApi(token=main_token)  # подключение токена \ авторизация бота
 longpoll = VkBotLongPoll(vk_session, 205640140)  # грубо говоря само подключение \ работа с сообщениями
@@ -26,18 +26,12 @@ for event in longpoll.listen():
             msg = event.obj['message']['text']
             print("Пользователь написал - ", msg)
             # username = get_user_name(event.message.from_id) # не работает надо придумать как пофиксить
-            if msg == '[club205640140|test_bot_ssau] help':
+            if msg == COMMAND_HELP:
                 sender(id, 'sometext')
             # Если приходит сообщение url=https://ssau.ru/rasp?groupId=531874164&selectedWeek=2&selectedWeekday=1
             elif msg[:4] == COMMAND_INPUT_URL:
                 sender(id, Parser(msg[4:]).get_weekly_schedule())
+            else:
+                sender(id, 'wtf')
 
-'''
-1)нужно добавить список команд
-2)есть проблемы с тем чтобы получать id пользователя который сделал зопрос(можно и без этого)
-3)не понятно насколько нормально отслеживать команды подобным образом [club205640140|test_bot_ssau] <название команды>
-если есть другой способ нужно его рассматреть
-4)переписать все по отдельным классам
-5)пропистаь команды в enum class 
-'''
 
--- main/bot/bot_command.py
+++ main/bot/bot_command.py
@@ -1,6 +1,6 @@
-__all__ = ('COMMAND_INPUT_URL', 'COMMAND_GET_PICTURE', 'COMMAND_SCHEDULE_ON_WEEK', 'COMMAND_SCHEDULE_NOW',)
+__all__ = ('COMMAND_INPUT_URL', 'COMMAND_SCHEDULE_ON_WEEK','COMMAND_HELP', )
 BOT_NAME = '[club205640140|test_bot_ssau]'
 COMMAND_INPUT_URL = BOT_NAME + ' url='
 COMMAND_SCHEDULE_ON_WEEK = BOT_NAME + ' расписание на неделю'
-COMMAND_GET_PICTURE = 'Бот, покажи картинку про'
-COMMAND_SCHEDULE_NOW = 'Бот, какое сейчас расписание?'
+COMMAND_HELP = BOT_NAME + ' help'
+
